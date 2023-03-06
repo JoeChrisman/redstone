fef16cc25d7e21d6df2c87f3ff5be0845b8a4af2
JoeChrisman <77989570+JoeChrisman@users.noreply.github.com>
2023-03-05 18:15:45 -0800

    removed unnecessary semicolons in rectangle.js

--- rectangle.js
+++ rectangle.js
@@ -12,18 +12,18 @@ export default class Rectangle {
         radii,
         isHighlighted
     ) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.bodyColor = bodyColor;
-        this.selectedBodyColor = selectedBodyColor;
-        this.borderColor = borderColor;
-        this.selectedBorderColor = selectedBorderColor;
-        this.borderWidth = borderWidth;
-        this.radii = radii;
-        this.isHighlighted = isHighlighted;
-    };
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.bodyColor = bodyColor
+        this.selectedBodyColor = selectedBodyColor
+        this.borderColor = borderColor
+        this.selectedBorderColor = selectedBorderColor
+        this.borderWidth = borderWidth
+        this.radii = radii
+        this.isHighlighted = isHighlighted
+    }
 
     copyRectangle() {
         return new Rectangle(
@@ -45,23 +45,23 @@ export default class Rectangle {
         return (this.x <= x &&
                 this.y <= y &&
                 this.x + this.width >= x &&
-                this.y + this.height >= y);
+                this.y + this.height >= y)
     }
 
     render(renderer) {
         // render the body of the rectangle
-        renderer.beginPath();
+        renderer.beginPath()
         renderer.roundRect(
             this.x,
             this.y,
             this.width,
             this.height,
-            this.radii);
-        renderer.fillStyle = this.isHighlighted ? this.selectedBodyColor : this.bodyColor;
-        renderer.fill();
+            this.radii)
+        renderer.fillStyle = this.isHighlighted ? this.selectedBodyColor : this.bodyColor
+        renderer.fill()
         // render the border of the rectangle
-        renderer.strokeStyle = this.isHighlighted ? this.selectedBorderColor : this.borderColor;
-        renderer.lineWidth = this.borderWidth;
-        renderer.stroke();
-    };
+        renderer.strokeStyle = this.isHighlighted ? this.selectedBorderColor : this.borderColor
+        renderer.lineWidth = this.borderWidth
+        renderer.stroke()
+    }
 }
